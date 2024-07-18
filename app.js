6871dcd73cc18415a85f973b1e6f84e23ec0cdfd
surajvish1 <surajvish101@gmail.com>
2024-07-18 16:14:13 +0530

    Updated sentence

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, This is my First deployment in AWS'));
+app.get('/', (req, res) => res.send('Hello World!, This is my First deployment in AWS, My CICD Pipeline was successfull'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
