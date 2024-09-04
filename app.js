68bf4409e180fef2724999f198e63130214496f5
SURAJ VISHWAKARMA <61005929+surajvish1@users.noreply.github.com>
2024-09-04 11:49:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, This is my First deployment in AWS, My CICD Pipeline was successfull'));
+app.get('/', (req, res) => res.send('Hello World!, This is my First deployment in AWS, My CICD Pipeline was successfull
+                                    
+                                    We are dedicated to delivering the highest quality products and services, underpinned by a deep understanding of your needs and aspirations. Our team works tirelessly to ensure that every interaction is marked by professionalism, respect, and a genuine desire to exceed your expectations'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
